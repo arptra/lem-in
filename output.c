13bd921b69ce5fe8c9b224a70e5ce5d1050e66ec
artyom <you@example.com>
2020-03-14 13:34:41 +0300

    ver 1.0, output suport only ants moves, fix

--- output.c
+++ output.c
@@ -61,7 +61,7 @@ void    output(t_paths *paths, t_graph *graph)
     j = 85;
     ants = init_ants();
     moves(paths);
-    while (j--)
+    while (paths->num_moves--)
     {
         i = put_ant(paths, ants, i);
         print_line(ants);
