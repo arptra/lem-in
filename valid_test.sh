70f2ffbeaebf5238a361348c0bd41fda543fb834
ai-asp <dukslis777@gmail.com>
2020-03-18 19:27:59 +0300

    make tester

--- valid_test.sh
+++ valid_test.sh
@@ -1,4 +1,21 @@
 #!/bin/bash
+i=0;
+dir="complexity_check/*"
+for file in $dir
+do
+EXP=$(head -n2 $file | grep "#" |cut -d':' -f2)
+GET=$(./lem-in < $file | grep L | wc -l)
+if [ "$GET" -le "$EXP" ]
+then
+echo -en "\t\033[32m \033[32m TEST NUMBER $i OK \033[32m \033[32m  \033[0m\n"
+else
+echo -en "\033[31m \033[31m TEST NUMBER $i -> shoud be: $EXP get from lem-in: $GET \033[0m\n"
+fi
+i=$((i+1));
+done
+
+
+cp lem-in 42_lem-in_tools
 dir="maps/validation_errs/*"
 i=1;
 for file in $dir
@@ -30,7 +47,6 @@ i=$((i+1))
 done
 fi
 
-cp lem-in 42_lem-in_tools
 cd 42_lem-in_tools
 dir="../valid_tests/*"
 i=1
