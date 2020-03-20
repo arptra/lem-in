5013b6fe346c1030ede4a98e58c8a9feb50f0a7d
ai-asp <dukslis777@gmail.com>
2020-03-20 16:06:23 +0300

    add checker

--- README.md
+++ README.md
@@ -19,3 +19,22 @@ $ pip3 install pyside2
 ```
 $ ./lem-in maps/theta_unequal_4.map | python3 lemin42-visual/lemin_visual.py
 ```
+# test lem-in
+
+```
+$ make && cp lem-in checker && cd checker && bash checker_lem-in.sh -abcdef
+```
+$ -h    show options with brief descriptions
+```
+$ -a    check errors management
+```
+$ -b    check leak with invalid map
+```
+$ -c    check output is right format
+```
+$ -d    check leak with valid map
+```
+$ -e    check time complexity
+```
+$ -f    check accuracy
+```
--- checker/check_lem-in.sh
+++ checker/check_lem-in.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-cp lem-in 42_lem-in_tools
-dir="maps/validation_errs/*"
+check_errors(){
+echo -e "\n\e[1;34mERROR MANAGEMENT TESTS\e[0m\n"
+dir="validation_errs/*"
 i=1;
 for file in $dir
 do
@@ -15,11 +16,12 @@ echo -en "test #:$i NOT PASS filename: $test_name \033[31m \033[31m KO \033[0m\n
 fi
 i=$((i+1))
 done
+}
 
-dir="maps/validation_errs/*"
+check_leak_errors(){
+echo -e "\n\e[1;34mMEMORY LEAK WITH INVALID MAP TESTS\e[0m\n"
+dir="validation_errs/*"
 i=1
-if [ "$1" = $"--leak-invalid" ]
-then
 echo -en "\t\033[32m \033[32m LOOK FOR LEAK \033[32m \033[32m  \033[0m\n"
 for file in $dir
 do
@@ -27,6 +29,17 @@ echo -en "\t\033[32m \033[32m TEST NUMBER $i $file \033[32m \033[32m  \033[0m\n"
 valgrind 2>&1 --leak-check=full --show-leak-kinds=all ./lem-in < $file | grep "LEAK SUMMARY:" -A5 | awk -F":" '{print $1 $2}'
 i=$((i+1))
 done
+}
+
+check_ouput(){
+echo -e "\n\e[1;34mLEM_IN'S OUTPUT CHECK\e[0m\n"
+
+if [ -d $"42_lem-in_tools" ]
+then
+    cp lem-in 42_lem-in_tools
+else
+    git clone https://github.com/apft/42_lem-in_tools.git
+    cp lem-in 42_lem-in_tools
 fi
 
 cd 42_lem-in_tools
@@ -38,20 +51,23 @@ echo -en "\t\033[32m \033[32m test #:$i  $file\033[32m \033[32m  \033[0m\n"
 bash checker.sh lem-in $file
 i=$((i+1))
 done
+cd ../
+}
 
+check_leak_valid(){
+echo -e "\n\e[1;34mMEMORY LEAK WITH VALID MAP TESTS\e[0m\n"
 i=1
-if [ "$2" = $"--leak-valid" ]
-then
-echo -en "\t\033[32m \033[32m LOOK FOR LEAK \033[32m \033[32m  \033[0m\n"
+dir="valid_tests/*"
 for file in $dir
 do
 echo -en "\t\033[32m \033[32m TEST NUMBER $i $file \033[32m \033[32m  \033[0m\n"
 valgrind 2>&1 --leak-check=full --show-leak-kinds=all ./lem-in < $file | grep "LEAK SUMMARY:" -A5 | awk -F":" '{print $1 $2}'
 i=$((i+1))
 done
-fi
-cd ../
+}
 
+check_complexity(){
+echo -e "\n\e[1;34mCHECK COMPLEXITY\e[0m\n"
 i=0;
 dir="time_check/*"
 for file in $dir
@@ -62,15 +78,16 @@ end=$(($(date +%s%N)/1000000))
 time=$((end-start))
 if [ "$time" -le $"3000" ]
 then
-echo -en "\t\033[32m \033[32m TEST NUMBER $i OK \033[32m \033[32m  \033[0m\n"
-echo -en "\t\033[32m \033[32m Execution time in miliseconds: $((end-start)) \033[32m \033[32m  \033[0m\n"
+echo -en "TEST #$i: Execution time in miliseconds = \033[32m\033[32m$((end-start))\t OK \033[32m \033[32m  \033[0m\n"
 else
-echo -en "\033[31m \033[31m TEST NUMBER $i -> time more than 3000 milisec -> $((end-start)) milisec\033[0m\n"
+echo -en "TEST #$i -> time more than 3000 milisec -> \033[31m \033[31m $((end-start)) milisec\033[0m\n"
 fi
 i=$((i+1));
 done
+}
 
-
+chcek_accuracy(){
+echo -e "\n\e[1;34mCHECK ACCURACY\e[0m\n"
 i=0;
 dir="accuracy_check/*"
 for file in $dir
@@ -79,10 +96,38 @@ EXP=$(head -n2 $file | grep "#" |cut -d':' -f2)
 GET=$(./lem-in < $file | grep L | wc -l)
 if [ "$GET" -le "$EXP" ]
 then
-echo -en "\t\033[32m \033[32m TEST NUMBER $i OK \033[32m \033[32m  \033[0m\n"
-echo -en "\t\033[32m \033[32m shoud be: $EXP get from lem-in: $GET \033[32m \033[32m  \033[0m\n"
+echo -en "test #$i shoud be: $EXP \033[32m\033[32m get from lem-in: $GET \033[32m\033[32m \033[0m\n"
 else
 echo -en "\033[31m \033[31m TEST NUMBER $i -> shoud be: $EXP get from lem-in: $GET \033[0m\n"
 fi
 i=$((i+1));
+done
+}
+
+print_usage(){
+echo "options:"
+echo "-h    show options with brief descriptions"
+echo "-a    check errors management"
+echo "-b    check leak with invalid map"
+echo "-c    check output is right format"
+echo "-d    check leak with valid map"
+echo "-e    check time complexity"
+echo "-f    check accuracy"
+exit 0
+}
+
+script_name="lem-in checker"
+
+while getopts 'habcdef' flag; do
+  case "${flag}" in
+    a)  check_errors ;;
+    b)  check_leak_errors ;;
+    c)  check_ouput ;;
+    d)  check_leak_valid ;;
+    e)  check_complexity ;;
+    f)  chcek_accuracy ;;
+    h)  print_usage ;;
+    *)  print_usage
+    exit 1 ;;
+  esac
 done
